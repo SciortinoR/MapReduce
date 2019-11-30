fe86932541e662dced69e7c583cc4514da111777
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-11-30 18:38:31 -0500

    Fixed dataset paths

--- src/mapper.py
+++ src/mapper.py
@@ -4,9 +4,9 @@ import pandas as pd
 # Determine which dataframe to load
 data = pd.DataFrame()
 if sys.argv[1] == "train":
-    data = pd.read_csv("datasets/NDBench-training.csv")
+    data = pd.read_csv("../datasets/NDBench-training.csv")
 elif sys.argv[1] == "test":
-    data = pd.read_csv("datasets/NDBench-testing.csv")
+    data = pd.read_csv("../datasets/NDBench-testing.csv")
 else:
     print("No dataset specified. Please specify 'rain' or 'test' for the respective dataset.")
     exit(0)
