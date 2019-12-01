e6057d9fc17500dcfda6766588fe504cbeec97cf
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-12-01 13:52:26 -0500

    Used stdin for compliance with hadoop

--- src/mapper.py
+++ src/mapper.py
@@ -1,16 +1,9 @@
 import sys
 import pandas as pd
 
-# Determine which dataframe to load
-data = pd.DataFrame()
-if sys.argv[1] == "train":
-    data = pd.read_csv("../datasets/NDBench-training.csv")
-elif sys.argv[1] == "test":
-    data = pd.read_csv("../datasets/NDBench-testing.csv")
-else:
-    print("No dataset specified. Please specify 'rain' or 'test' for the respective dataset.")
-    exit(0)
+# Load data
+data = pd.read_csv(sys.stdin)
 
 # Prints/Passes values to reducer.py
-for value in data[sys.argv[2]].values.tolist():
+for value in data[sys.argv[1]].values.tolist():
     print('%s' % value)
