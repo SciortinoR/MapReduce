8ed88f8d7a65b3443a6b7997852fe730914b3154
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-12-03 15:33:22 -0500

    Added fieldnames for MapReduce splitting of dataset

--- src/mapper.py
+++ src/mapper.py
@@ -3,8 +3,8 @@ import sys
 import csv
 
 # Load data
-data = csv.DictReader(sys.stdin)
+data = csv.DictReader(sys.stdin, fieldnames=("CPUUtilization_Average", "NetworkIn_Average", "NetworkOut_Average", "MemoryUtilization_Average", "Final_Target"))
 
-# Prints/Passes values to reducer.py
+# Prints/Passes key-value (col-value) to reducer.py
 for row in data:
-    print('%s' % row[sys.argv[1]])
+    print('%s\t%s' % (sys.argv[1], row[sys.argv[1]]))
