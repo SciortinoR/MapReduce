e79496d7819e3281d6ce0c45ceb23e77d346e68e
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-12-03 15:33:52 -0500

    Updated to reducer format

--- src/reducer.py
+++ src/reducer.py
@@ -2,21 +2,34 @@
 import sys
 import statistics
 
+key = None
+current_key = None
 num_list = []
-
-for value in sys.stdin:
+for line in sys.stdin:
     # Remove leading and trailng whitespace
-    value = value.strip()
+    line = line.strip()
 
-    # Convert string to int
+    # Parse input 
+    key, value = line.split('\t', 1)
+
+    # Convert string to float
     try:
         value = float(value)
     except ValueError:
         # Skip the value
         continue
 
-    # Append number and calculate stats for every iteration
-    num_list.append(value)
-    print("Iteration %s\t --> Max: %s\t Min: %s\t Median: %s\t Standard Deviation: %s" \
-        % (len(num_list), max(num_list), min(num_list), statistics.median(num_list), statistics.pstdev(num_list)))
+    if current_key == key:
+        num_list.append(value)
+    else:
+        if current_key:
+            print("Column: %s\t # Data Points: %s\t Max: %s\t Min: %s\t Median: %s\t Standard Deviation: %s" \
+                % (current_key, len(num_list), max(num_list), min(num_list), statistics.median(num_list), statistics.pstdev(num_list)))
+        num_list.clear()
+        num_list.append(value)
+        current_key = key
 
+# Output last value if needed
+if current_key == key:
+    print("Column: %s\t # Data Points: %s\t Max: %s\t Min: %s\t Median: %s\t Standard Deviation: %s" \
+                % (current_key, len(num_list), max(num_list), min(num_list), statistics.median(num_list), statistics.pstdev(num_list)))
