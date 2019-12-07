284dfe8c6c95cde4c4f6eab886aa0a50c51172f7
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-12-07 17:13:18 -0500

    Added 90th percentile calculation + cleaned code

--- src/reducer.py
+++ src/reducer.py
@@ -2,11 +2,22 @@
 import sys
 import statistics
 
+def percentile90(num_list):
+    n = max(int(round(0.9 * len(num_list) + 0.5)), 2)
+    return num_list[n-2]
+
 def print_stats(current_key, num_list):
+    num_list.sort()
     maximum = max(num_list)
     minimum = min(num_list)
-    print("Column: %s\t # Data Pts.: %s\t Max: %s\t Min: %s\t Median: %s\t Std. Dev.: %s\n" \
-                % (current_key, len(num_list), maximum, minimum, statistics.median(num_list), statistics.pstdev(num_list)))
+
+    print("Column: %s" % current_key)
+    print("# of Samples: %s" % len(num_list))
+    print("Maximum: %s" % maximum)
+    print("Minimum: %s" % minimum)
+    print("Median: %s" % statistics.median(num_list))
+    print("Standard Deviation: %s" % statistics.pstdev(num_list))
+    print("Value at or below which 90%% of samples are found (90th Percentile): %s\n" % percentile90(num_list))
     
     print("Normalized column values: ")
     for value in num_list:
